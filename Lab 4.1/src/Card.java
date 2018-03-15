a277d15ae06651ae79ea10c08323b3a2ce7f4be1
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2018-03-15 09:07:45 -0400

    d

--- Lab 4.1/src/Card.java
+++ Lab 4.1/src/Card.java
@@ -1,9 +1,12 @@
 
 public class Card 
 {
-	public Card(String rank, String suit, int pointValue)
+	int[] deck = new int[52];
+	String [] suit = {"Diamond", "Clubs", "Hearts", "Spades"};
+	String [] rank = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+	
+	public Card(String rank, String suit, int value)
 	{
 		
 	}
-
 }
--- Lab 4.1/src/Deck.java
+++ Lab 4.1/src/Deck.java
@@ -1,11 +1,16 @@
+import java.util.ArrayList;
 
 public class Deck 
 {
+	ArrayList <Card> unDelt;
 	public Deck(String [] rank, String [] suit, int[] value)
 	{
 		for(int i = 0; i < rank.length; i++) 
 		{
-			for(int j = 0;)
+			for(int j = 0; j < suit.length; j++)
+			{
+				unDelt.add(new Card(rank[i], suit[i], value[i]));
+			}
 		}
 	}
 
